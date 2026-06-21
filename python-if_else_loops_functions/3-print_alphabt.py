407ce6dd50f123be5ae04f3aaa7fc51dfaffffdf
marvellio77 <m.munezero@alustudent.com>
2026-06-21 21:23:29 +0200

    Fix 3-print_alphabt.py: wrap long line for PEP8 line-length

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print(("{}" * 24).format(*(chr(i) for i in range(97, 123) if chr(i) != "q" and chr(i) != "e")), end="")
+print(("{}" * 24).format(
+    *(chr(i) for i in range(97, 123) if chr(i) != "q" and chr(i) != "e")
+), end="")
