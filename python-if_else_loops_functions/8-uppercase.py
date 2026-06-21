fe5e42eb053c0e0b6e4834e660e656a574bd516b
marvellio77 <m.munezero@alustudent.com>
2026-06-21 21:45:38 +0200

    Add 8-uppercase.py: converts string to uppercase using ord/chr

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module that prints a string in uppercase."""
+
+
+def uppercase(str):
+    """Print str in uppercase, followed by a newline."""
+    result = ""
+    for c in str:
+        if ord("a") <= ord(c) <= ord("z"):
+            result += chr(ord(c) - 32)
+        else:
+            result += c
+    print(result)
