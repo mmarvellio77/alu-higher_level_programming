34357f7bbf6acb2461d00d9fbf9afff928e06cec
marvellio77 <m.munezero@alustudent.com>
2026-07-07 01:54:38 +0200

    Add 6-raise_exception_msg.py: raise a NameError with custom message

---
+++ python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that defines raise_exception_msg"""
+
+
+def raise_exception_msg(message=""):
+    """Raises a NameError exception with a given message"""
+    raise NameError(message)
