87826c43f93b4f9dcab790a4a07e909f3c688cae
marvellio77 <m.munezero@alustudent.com>
2026-07-06 20:30:19 +0200

    Fix PEP8 line length issue in 10-divisible_by_2.py

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@
 
 
 def divisible_by_2(my_list=[]):
-    """Returns a list of booleans indicating if each integer is divisible by 2"""
+    """Returns list of booleans for divisibility by 2"""
     result = []
     for number in my_list:
         result.append(number % 2 == 0)
