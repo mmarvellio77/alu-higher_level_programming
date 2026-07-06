679e2b9ed4e636d689db557365a2702178136430
marvellio77 <m.munezero@alustudent.com>
2026-07-07 01:52:27 +0200

    Add 5-raise_exception.py: raise a TypeError exception

---
+++ python-exceptions/5-raise_exception.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that defines raise_exception"""
+
+
+def raise_exception():
+    """Raises a TypeError exception"""
+    raise TypeError("This is a TypeError exception")
