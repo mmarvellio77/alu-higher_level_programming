d68c6029029571cb0018fe8242c134765b84d928
marvellio77 <m.munezero@alustudent.com>
2026-07-18 15:26:44 +0200

    Fix 7-base_geometry: handle bool and add missing test cases

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -11,7 +11,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """Validates that value is a positive integer."""
-        if type(value) is not int:
+        if type(value) is not int or isinstance(value, bool):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
