b017074f91b644a54e1ca4c599b388114e04a52b
marvellio77 <m.munezero@alustudent.com>
2026-07-28 21:36:45 +0200

    Fix MySQL 8.0 syntax for tasks 0 and 1

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
-cat << 'EOF' > 0-privileges.sql
 -- Lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
-EOF
--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,3 @@
-cat << 'EOF' > 1-create_user.sql
 -- Creates the MySQL server user user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-FLUSH PRIVILEGES;
-EOF
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
