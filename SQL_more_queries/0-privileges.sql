5a6bbbe67bea2c50c7aecb2ea8a7417abd47e016
marvellio77 <m.munezero@alustudent.com>
2026-07-29 10:07:50 +0200

    Edit 0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,8 @@
--- Lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server
+-- List users to verify they exist
+SELECT user, host FROM mysql.user WHERE user IN ('user_0d_1', 'user_0d_2');
+
+-- Show privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Show privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
