844aa8bd8dae820e2b299b3a129b2cc39629a716
marvellio77 <m.munezero@alustudent.com>
2026-07-30 16:31:12 +0200

    4-hbtn_status.py: fix PEP8 line length

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status using requests and displays the response body."""
+"""Fetches https://intranet.hbtn.io/status and displays the body."""
 import requests
 
 
