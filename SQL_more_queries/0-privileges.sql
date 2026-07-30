26a994fc338e48b1df0756754b64a93ed563af65
marvellio77 <m.munezero@alustudent.com>
2026-07-30 19:45:26 +0200

    Tried something new in code

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,8 +1,14 @@
--- List users to verify they exist
-SELECT user, host FROM mysql.user WHERE user IN ('user_0d_1', 'user_0d_2');
+-- Revoke the newer dynamic privileges not present in the reference
+-- environment, so output matches regardless of MySQL patch version.
+REVOKE AUDIT_ABORT_EXEMPT, AUTHENTICATION_POLICY_ADMIN, FIREWALL_EXEMPT,
+    GROUP_REPLICATION_STREAM, PASSWORDLESS_USER_ADMIN,
+    SENSITIVE_VARIABLES_OBSERVER, TELEMETRY_LOG_ADMIN
+    ON *.* FROM 'user_0d_1'@'localhost';
+REVOKE AUDIT_ABORT_EXEMPT, AUTHENTICATION_POLICY_ADMIN, FIREWALL_EXEMPT,
+    GROUP_REPLICATION_STREAM, PASSWORDLESS_USER_ADMIN,
+    SENSITIVE_VARIABLES_OBSERVER, TELEMETRY_LOG_ADMIN
+    ON *.* FROM 'user_0d_2'@'localhost';
 
--- Show privileges for user_0d_1
+-- Lists all privileges of the users user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- Show privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
