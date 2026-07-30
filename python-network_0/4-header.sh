e62624adb3935e66ff89894fb96cc44871c75391
marvellio77 <m.munezero@alustudent.com>
2026-07-30 11:50:56 +0200

    restructure last line of 4-header.sh

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request with a custom header and displays the body
-curl -sL -H "X-HolbertonSchool-User-Id: 98" "$1"
+# display the body of a url call 
+curl -sG "$1" -H "X-School-User-Id: 98"
