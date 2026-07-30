6e9e6bf770b42a41797c9692b2983a2edacd155f
marvellio77 <m.munezero@alustudent.com>
2026-07-30 14:27:54 +0200

    4-hbtn_status.py: fetch status using requests

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Fetches https://intranet.hbtn.io/status using requests and displays the response body."""
+import requests
+
+
+if __name__ == "__main__":
+    response = requests.get("https://intranet.hbtn.io/status")
+    content = response.text
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
