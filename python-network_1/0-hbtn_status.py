e837fed4875b4a56dbe520c6dbc8bae0abf826b2
marvellio77 <m.munezero@alustudent.com>
2026-07-30 12:08:27 +0200

    0-hbtn_status.py: fix hostname to intranet.hbtn.io

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
-"""A script that
-- Fetches https://intranet.hbtn.io/status and displays the response body.
-- uses urlib package
-"""
+"""Fetches https://intranet.hbtn.io/status and displays the response body."""
+from urllib import request
 
-import urllib.request
 
-url = 'https://intranet.hbtn.io/status'
-if url.startswith('https://'):
-    url = 'https://alu-intranet.hbtn.io/status'
-
-if __name__ == '__main__':
-    with urllib.request.urlopen(url) as res:
-        content = res.read()
+if __name__ == "__main__":
+    with request.urlopen("https://intranet.hbtn.io/status") as response:
+        body = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
